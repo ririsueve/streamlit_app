433e9140613be868e3e6fed3a7b01d6d30ce73ed
ririsueve <ngocdobentre@gmail.com>
2026-05-27 19:17:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # 1. Cài đặt tiêu đề trang web
-st.set_page_config(page_title="App Tính Tổng - Khung Nét Đứt", layout="centered")
+st.set_page_config(page_title="App Tính Tổng", layout="centered")
 
 # 2. Đưa đoạn CSS font Montserrat & màu XANH DƯƠNG vào
 custom_css = """
